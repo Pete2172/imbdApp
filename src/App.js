8edbf162222f5bc4194e26b7acaed85a564be71a
Piotr Kowalczyk <p.kowalczyk.95@wp.pl>
2020-01-13 13:26:56 +0100

    Added component MoviePage, which displays detailed movie info

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MovieSearch from './components/MovieSearch.js';
+import MoviePage from './components/MoviePage.js';
 
 function App() {
 
   const [search, setSearch] = useState("");
+  //<MovieSearch title={search} onClickHandler={() => {console.log("clicked!")}} />
 
   return (
     <div className="App">
       <input type="text" value={search} placeholder="Type a title of a movie..." onChange={e => setSearch(e.target.value)} />
-      <MovieSearch title={search} onClickHandler={() => {console.log("clicked!")}} />
+      <MoviePage />
     </div>
   );
 }
