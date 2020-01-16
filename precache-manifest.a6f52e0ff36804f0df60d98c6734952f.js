a341960158a4bf4331386e876b234dee0f169813
Piotr Kowalczyk <p.kowalczyk.95@wp.pl>
2020-01-16 20:58:16 +0100

    Updates

---
+++ precache-manifest.a6f52e0ff36804f0df60d98c6734952f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92447cf29cd94d29f122ebca8bc495f1",
+    "url": "/imbdApp/index.html"
+  },
+  {
+    "revision": "55752bcb75f86539ca91",
+    "url": "/imbdApp/static/css/main.34451713.chunk.css"
+  },
+  {
+    "revision": "3d6c24298e8041447445",
+    "url": "/imbdApp/static/js/2.5f2b8a94.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/imbdApp/static/js/2.5f2b8a94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55752bcb75f86539ca91",
+    "url": "/imbdApp/static/js/main.4c0cff2a.chunk.js"
+  },
+  {
+    "revision": "50b985dce22b0a2b1bb2",
+    "url": "/imbdApp/static/js/runtime-main.757f072f.js"
+  }
+]);
